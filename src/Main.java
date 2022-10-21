fd79710ebf1535a71515ca0c5aff12bf600b752a
rodi1022 <roshini.diddi@netcracker.com>
2022-10-21 11:20:08 +0530

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+package mx.tc.j2se.tasks;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Task t1=new Task("Morning Run",3,15,2);
+        Task t2=new Task("Reading Book",8,10,2);
+        Task t3=new Task("Playing badminton",9);
+        /***if(t1.isRepeated()){
+            t1.current=t1.start;
+            while(t1.current< t1.getEndTime()) {
+                System.out.println(t1.getTitle()+ " " + t1.getCurrentTime());
+                t1.current += t1.getRepeatInterval();
+                if(t1.current < t1.getEndTime()){
+                    t1.setActive(true);}
+                else{t1.setActive(false);}
+                System.out.println(t1.isActive());
+            }
+        }**/
+        ArrayTaskList atl=new ArrayTaskList();
+        atl.add(t1);
+        atl.add(t2);
+        atl.add(t3);
+        atl.display();
+        //atl.remove(t2);
+        //atl.display();
+        //System.out.println(atl.size());
+        //System.out.println(atl.getTask(1).getTitle());
+
+        atl.incoming(5,10).display();
     }
 }
--- src/mx/tc/j2se/tasks/ArrayTaskList.java
+++ src/mx/tc/j2se/tasks/ArrayTaskList.java
@@ -1,2 +1,64 @@
-package mx.tc.j2se.tasks;public class ArrayTaskList {
+package mx.tc.j2se.tasks;
+
+
+import java.util.Arrays;
+
+public class ArrayTaskList<task> {
+    private static final int INITIAL_CAPACITY = 10;
+    private int size = 0;
+    private Task elementData[] = {};
+    public ArrayTaskList(){
+        elementData = new Task[INITIAL_CAPACITY];
+
+    }
+    /*
+    this method adds the task to the ArrayTaskList
+     */
+    public void add (Task task){
+        if(size == elementData.length){
+            ensureCapacity();
+        }
+        elementData[size++]=task;
+    }
+    boolean remove (Task task){
+        for(int i=0;i<size;i++){
+            if(elementData[i].getTitle()==task.getTitle()){
+                for(int j=i;j<size;j++)
+                {
+                    elementData[j]=elementData[j+1];
+                }
+                size--;
+                break;
+            }
+        }
+        return true;
+    }
+    public int size(){
+        return this.size;
+    }
+    public Task getTask(int index){
+        return elementData[index];
+    }
+    public void display() {
+        System.out.print("Displaying list : ");
+        for (int i = 0; i < size; i++) {
+            //System.out.println(elementData[i].getTitle() + " "+ elementData[i].getStartTime()+" "+elementData[i].isRepeated());
+            System.out.println(elementData[i].getTitle());
+        }
+    }
+
+    private void ensureCapacity() {
+        int newIncreasedCapacity = elementData.length * 2;
+        elementData= Arrays.copyOf(elementData,newIncreasedCapacity);
+    }
+    public ArrayTaskList incoming(int from, int to){
+        ArrayTaskList res=new ArrayTaskList();
+        for(int i=0;i<size;i++){
+            if(elementData[i].getStartTime()>=from && elementData[i].getEndTime()<=to){
+                res.add(elementData[i]);
+            }
+        }
+        return res;
+    }
+
 }
